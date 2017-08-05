ba65748cb3728b06f08b4ac6f8c4eecf2b2640c7
hbtothemax <henrybosch15@gmail.com>
2017-08-05 15:25:40 -0400

    Update MusicPlot.py

--- song_fingerprint/MusicPlot.py
+++ song_fingerprint/MusicPlot.py
@@ -342,3 +342,40 @@ def addSong(name, fileAddress):
 
     add_song_to_dict(peaksd, name)
     print(name + ' song added')
+
+
+def MicCheck(length):
+    """
+    :param length: how long you want to record for - int
+    :return: perceentage confidence of song
+    """
+    #print(length)
+    length = (int(length))
+
+    #return statement(length)
+    MicPeaks = mic_to_numpy_array(length)
+    print("Mic reocrded")
+    #return statement("checl")
+    Stemp, f, t = spectogram(MicPeaks)
+    MicPeaksTemp = peaks(Stemp)
+    print("Mic peaks gotten")
+    peaksMicFinal = convertData(MicPeaksTemp)
+    print("Mic data converted")
+
+    #return statement('dipppp')
+    temp =  check_database(peaksMicFinal, length)
+
+    if temp[1] < .32:#32
+        return statement("I am not sure")
+    else:
+        return statement("I am " + str(temp[1]) + " percent confident that the song is " + str(temp[0]) + '.')
+    #print(returnn)
+    #if returnn == "I am not sure":
+    #    msg = returnn
+    #else:
+    #    msg = "I am " + returnn[1] + " percent confident that the song is " + returnn[0] + '.'
+    #print(msg)
+    #print(type(returnn))
+    #print(type(msg))
+    #return statement("asdasdasdasdsdasd")
+    #return statement(str(length))
