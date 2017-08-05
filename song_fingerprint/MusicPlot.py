a75ddf230cbe4dc265699bacf6a7004975cc4ff5
Henry Bosch <henry@dhcp-18-111-56-21.dyn.mit.edu>
2017-08-05 14:31:48 -0400

    More changes, finishing functionality of songfp package.

--- song_fingerprint/MusicPlot.py
+++ song_fingerprint/MusicPlot.py
@@ -345,3 +345,60 @@ def addSong(name, fileAddress):
 
 
 # song_database
+
+####
+
+
+
+
+
+#####
+@ask.intent("RecordSong")
+def MicCheck(length):
+    """
+
+    :param length: how long you want to record for - int
+    :return: perceentage confidence of song
+    """
+    print(length)
+    length = (int(length))
+
+    #return statement(length)
+    MicPeaks = mic_to_numpy_array(length)
+    print("Mic reocrded")
+    #return statement("checl")
+    Stemp, f, t = spectogram(MicPeaks)
+    MicPeaksTemp = peaks(Stemp)
+    print("Mic peaks gotten")
+    peaksMicFinal = convertData(MicPeaksTemp)
+    print("Mic data converted")
+
+    #return statement('dipppp')
+    temp =  check_database(peaksMicFinal, length)
+
+    if temp[1] < .32:#32
+        return statement("I am not sure")
+    else:
+        return statement("I am " + str(temp[1]) + " percent confident that the song is " + str(temp[0]) + '.')
+    #print(returnn)
+    #if returnn == "I am not sure":
+    #    msg = returnn
+    #else:
+    #    msg = "I am " + returnn[1] + " percent confident that the song is " + returnn[0] + '.'
+    #print(msg)
+    #print(type(returnn))
+    #print(type(msg))
+    #return statement("asdasdasdasdsdasd")
+    #return statement(str(length))
+
+@app.route('/')
+def homepage():
+    return "Song FP"
+
+@ask.launch
+def start_skill():
+    msg = "how long would you like me to record for?"
+    return question(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
